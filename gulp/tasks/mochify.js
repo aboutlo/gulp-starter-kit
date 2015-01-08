25fa73364e35a89c7c146987564985c3802358d1
Lorenzo Sicilia <lorenzo@spatch.co>
2015-01-08 15:34:58 +0000

    Remove consolify from mochify task

--- gulp/tasks/mochify.js
+++ gulp/tasks/mochify.js
@@ -4,12 +4,12 @@ var gulp = require('gulp');
 var mochify = require('mochify');
 var config  = require('../config').js;
 
-gulp.task('mochify', function () {
+gulp.task('mochify', ['jshint'], function () {
   mochify( config.test, {
-    reporter : 'spec',
-    debug: true,
+    reporter : 'spec'
+    //debug: true,
     //cover    : true,
-    consolify : 'build/runner.html'
+    //consolify : 'build/runner.html'
     //TODO require  : 'chai' and expose expect   https://github.com/gulpjs/gulp/blob/master/docs/recipes/mocha-test-runner-with-gulp.md
   }).bundle();
 });
