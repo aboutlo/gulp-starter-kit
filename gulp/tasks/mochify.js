03d408714f001c291ea9ce97c4077ffceb0a94b7
Lorenzo Sicilia <lorenzo@spatch.co>
2015-01-09 13:35:39 +0100

    Put runner.html in test directory

--- gulp/tasks/mochify.js
+++ gulp/tasks/mochify.js
@@ -9,7 +9,7 @@ gulp.task('mochify', ['jshint'], function () {
     reporter : 'spec'
     //debug: true,
     //cover    : true,
-    //consolify : 'build/runner.html'
+    //consolify : 'test/runner.html'
     //TODO require  : 'chai' and expose expect   https://github.com/gulpjs/gulp/blob/master/docs/recipes/mocha-test-runner-with-gulp.md
   }).bundle();
 });
