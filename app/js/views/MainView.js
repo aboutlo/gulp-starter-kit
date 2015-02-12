d0a6ca4ef4b60db938359db346149c1b5f3d0860
Lorenzo Sicilia <lorenzo@spatch.co>
2015-02-12 21:17:41 +0000

    Create welcome page

--- app/js/views/MainView.js
+++ app/js/views/MainView.js
@@ -5,38 +5,23 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.$ = $;
 
-var EventBus = require('../utils/EventBus').EventBus();
-var Dispatcher = require('../utils/AppDispatcher');
-
 var MainView = Backbone.View.extend({
 
-  main: require('../templates/status.tpl'),
+  template: require('../templates/main.tpl'),
+
+  tagName:'section',
+  className:'main',
+
+  events:{
+    'click #login_button': 'login'
+  },
 
   initialize: function(){
-
-    console.log('MainView register to GlobalEvent');
-
-
-    //Dispatcher.register('GLOBAL:TEST',function(e){
-    //   console.log('MainView Dispatcher get', e);
-    //});
-
-
-
-    //EventBus.on('GLOBAL:TEST',function(e){
-    //  console.log('MainView get global event:' + e);
-    //});
-    //console.log(this.$el);
-    //this.$main = this.$el.find('#main');
+    this.listenTo(this.model,'change',this.render);
   },
 
   render: function(){
-    this.$el.html(this.main(this.model.attributes));
-
-
-    //EventBus.trigger('GLOBAL:TEST', { username:'user', password:'pswd'});
-
-
+    this.$el.html(this.template(this.model.attributes));
     return this;
   }
 
