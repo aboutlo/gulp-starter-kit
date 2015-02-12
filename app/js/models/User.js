642e6bfb3bafecf4efc14b1a352ecbe188ec1814
Lorenzo Sicilia <lorenzo@spatch.co>
2015-02-12 21:09:30 +0000

    Adding validation to User and his tests

---
+++ app/js/models/User.js
@@ -0,0 +1,60 @@
+'use strict';
+var $ = require('jquery');
+var Backbone = require('backbone');
+var Dispatcher = require('../utils/AppDispatcher');
+var Actions = require('../actions/Actions');
+var _ = require('underscore');
+Backbone.$ = $;
+
+module.exports = Backbone.Model.extend({
+
+  defaults:{
+    username: '',
+    password: '',
+    status:   'offline',
+    authenticated:   false
+  },
+
+  initialize: function(){
+    //console.log('User init');
+
+    Dispatcher.register(_.bind(function(payload){
+      switch (payload.actionType){
+
+        case Actions.USER_AUTHENTICATE:
+
+          this.set(payload.user, { validate:true });
+
+          if (this.isValid()){
+             this.authenticate();
+          }
+
+          break;
+
+        default:
+        //DONOTHING
+      }
+
+    },this));
+
+  },
+
+  validate: function(attributes){
+    var errors = [];
+    if (attributes.username.length < 6 || attributes.password.length < 5 ){
+      errors.push('Invalid credentials');
+    }
+    return errors.length === 0 ? null : errors;
+  },
+
+  // fake auth
+  authenticate:function(){
+    if (this.get('username') !== 'admin@foo.com' || this.get('password') !== 'admin'){
+      this.validationError = 'Wrong username or password';
+      this.set({authenticated:false},{silent:true});
+      this.trigger('invalid');
+    }
+    this.set({authenticated:true, status:'online'});
+  }
+
+});
