44bbfa946f1d91cbbe71db9b982fcc7639a36e2a
Lorenzo Sicilia <lorenzo@spatch.co>
2015-02-15 19:33:08 +0100

    Adding watch to mochify task only on development envintorment

--- gulp/tasks/mochify.js
+++ gulp/tasks/mochify.js
@@ -4,12 +4,18 @@ var gulp = require('gulp');
 var mochify = require('mochify');
 var config  = require('../config').js;
 
-gulp.task('mochify', ['jshint'], function () {
+
+gulp.task('mochify', ['jshint'], function (watch) {
+
+  var envinronment = process.env.NODE_ENV ||'development';
+
   mochify( config.test, {
-    reporter : 'spec'
+    reporter : 'spec',
+    watch: envinronment === 'development'
     //debug: true,
     //cover    : true,
     //consolify : 'test/runner.html'
     //TODO require  : 'chai' and expose expect   https://github.com/gulpjs/gulp/blob/master/docs/recipes/mocha-test-runner-with-gulp.md
   }).bundle();
+
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,11 +7,11 @@
 var gulp     = require('gulp');
 var config   = require('../config');
 
-gulp.task('watch', ['watchify','browserSync'], function(callback) {
+gulp.task('watch', ['watchify','browserSync', 'mochify'], function(callback) {
   gulp.watch(config.sass.src,   ['sass']);
   gulp.watch(config.images.src, ['images']);
   gulp.watch(config.markup.src, ['markup']);
   gulp.watch('gulp/**/*.js', ['jshint']);
-  gulp.watch([config.js.src, config.js.test ], ['mochify']);
+  //gulp.watch([config.js.src, config.js.test ], ['mochify']);
   // Watchify will watch and recompile our JS, so no need to gulp.watch it
 });
